99b81fdac793e93dc37e742821c7ad0acf478ff3
Saleem Ayoub khan <saleemayoub1@gmail.com>
2022-08-03 23:37:33 +0500

    added foreignkeys in migration

--- database/migrations/2022_08_03_144606_create_transactions_table.php
+++ database/migrations/2022_08_03_144606_create_transactions_table.php
@@ -18,6 +18,9 @@ class CreateTransactionsTable extends Migration
             $table->integer('quantity')->unsigned();
             $table->integer('buyer_id')->unsigned();
             $table->integer('product_id')->unsigned();
+            // $table->foreign('buyer_id')->references('id')->on('users');
+            // $table->foreign('product_id')->references('id')->on('products');
+
             $table->timestamps();
         });
     }
