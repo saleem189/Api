1179b572b28501823eb885ca63f29602931eda89
Saleem Ayoub khan <saleemayoub1@gmail.com>
2022-08-03 23:33:24 +0500

    added commit in api routes

--- routes/api.php
+++ routes/api.php
@@ -56,4 +56,4 @@ Route::resource('transaction',TransactionController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum'); //this line is fot only loged in usrs or who has login Barer code
