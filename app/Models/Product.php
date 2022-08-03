14efc87cac981923cc0448e18ddb517e475fb952
Saleem Ayoub khan <saleemayoub1@gmail.com>
2022-08-03 23:24:47 +0500

    first uploaded code

---
+++ app/Models/Product.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    const AVAILABLE_PRODUCT = 'available';
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+
+        
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        // 'password',
+        // 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        // 'email_verified_at' => 'datetime',
+    ];
+
+    public function isAvailable(){
+        return $this->status == Product::AVAILABLE_PRODUCT;
+    }
+
+    // public function isUnAvailable(){
+    //     return $this->status == Product::UNAVAILABLE_PRODUCT;
+    // }
+
+    public function seller(){
+        return $this->belongsTo(Seller::class);
+    }
+
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
+    public function transactions(){
+        return $this->hasMany(Transaction::class);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,59 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BuyerController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SellerController;
+use App\Http\Controllers\TransactionController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+/**
+ * commeting these default lines
+ */
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+/**
+ * Buyer Model Resource Controller
+ */
+Route::resource('buyer',BuyerController::class);
+
+
+/**
+ * Seller Model Resource Controller
+ */
+Route::resource('seller',SellerController::class);
+
+
+/**
+ * Product Model Resource Controller
+ */
+Route::resource('product',ProductController::class);
+
+/**
+ * Category Model Resource Controller
+ */
+Route::resource('category',CategoryController::class);
+
+/**
+ * Transaction Model Resource Controller
+ */
+Route::resource('transaction',TransactionController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
