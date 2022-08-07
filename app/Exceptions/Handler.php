b6403bba9cb1662b1d12277c4588ce6c1a943e04
Saleem Ayoub khan <saleemayoub1@gmail.com>
2022-08-07 18:51:08 +0500

    added custom exception handler for api and their responces

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Prophecy\Exception\Doubler\MethodNotFoundException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -28,6 +37,50 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * By default, the report method passes the exception to the base class where the exception is logged. However, you are free to log exceptions however you wish.
+     */
+    public function report(Throwable $exception)
+    {
+        parent::report($exception);
+    }
+    
+    public function render($request, Throwable $exception)
+    {
+        /**
+         * these Exception will only run for Api .. when header of ['Accept' : 'application/json'] is present and for web it will return laravel default validations
+         */
+        if ($request->wantsJson()) {
+            if($exception instanceOf ModelNotFoundException){
+                $modelName = strtolower(class_basename($exception->getModel()));
+                return $this->errorResponse("The record you are trying to access does not exit or it was deleted for {$modelName}",404);
+            }elseif($exception instanceOf NotFoundHttpException){
+                return $this->errorResponse('Page Not Found ',404);
+            }elseif($exception instanceOf AuthenticationException){
+                return $this->errorResponse('Unauthenticated please login',401);
+            }elseif($exception instanceOf AuthorizationException){
+                return $this->errorResponse('Sorry you are Unauthorized to perfom this action',403);
+            }elseif($exception instanceOf MethodNotAllowedHttpException){
+                return $this->errorResponse($exception->getMessage(),405);
+            }elseif($exception instanceOf QueryException){
+                $errorCode = $exception->errorInfo[1];
+                if ($errorCode == 1451) {
+                    return $this->errorResponse('Cannot remove this resource permanently. It is related with any other resource',409); //409  is conflict error
+                }
+                
+            }
+            else{
+
+                return $this->errorResponse('Unexpected Exception',500);  //generic Unexpected exception response
+            }
+        }
+        if (config('app.debug')) {
+            return parent::render($request, $exception);
+
+        }
+            // return parent::render($request, $exception);
+    }
+
+    /**
      * Register the exception handling callbacks for the application.
      *
      * @return void
@@ -37,5 +90,8 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+        
+
+        
     }
 }
